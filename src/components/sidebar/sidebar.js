e16b0c8adc8f325c27cbebaf242bf13dab1dcf67
cemkiy <cemekiy@gmail.com>
2018-03-19 17:49:49 +0300

    logo template

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './sidebar.css';
-import logo from '../../logo.svg';
+import logo from '../../logo.png';
 import { Menu, Image } from 'semantic-ui-react'
 
 export default class Sidebar extends Component {
