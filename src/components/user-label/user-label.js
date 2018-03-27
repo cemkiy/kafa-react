24336f19212a64cd47b874d406f1027c3024ccf3
cemkiy <cemekiy@gmail.com>
2018-03-27 09:53:39 +0300

    link fix

--- src/components/user-label/user-label.js
+++ src/components/user-label/user-label.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import { Router, Link } from 'react-router-dom'
 import satellite from '../../assets/img/satellite.gif';
 import { Label, Popup, Card, Image, Icon } from 'semantic-ui-react'
 
-export class UserLabel extends Component {
-  constructor(props) {
-    super(props);
-    this.redirectToProfile = this.redirectToProfile.bind(this);
-  }
-
-  redirectToProfile = () => {
-    this.props.history.push('/profile')
-  }
+export default class UserLabel extends Component {
 
   render() {
     return (
       <Popup
          trigger={
-           <Label as='a' onClick={this.redirectToProfile} color='yellow' image>
-             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3AHJGuP0nmqfIv4Z0lWketAX7Q1KF21RXSWo2CkIOq6DSep2Y' />
-             CaptainJack
-             <Label.Detail>User</Label.Detail>
-           </Label>
+             <a href='/profile'>
+               <Label color='yellow' image>
+                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3AHJGuP0nmqfIv4Z0lWketAX7Q1KF21RXSWo2CkIOq6DSep2Y' />
+                 CaptainJack
+                 <Label.Detail>User</Label.Detail>
+               </Label>
+             </a>
          }
          flowing
          hoverable>
@@ -49,5 +43,3 @@ export class UserLabel extends Component {
     )
   }
 }
-
-export default withRouter(UserLabel);
